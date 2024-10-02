1fcc34f61a0c373a1deba64821001e2e179fcfa0
waxie <97557771+waxiechc@users.noreply.github.com>
2024-10-02 20:47:43 +0300

    Add files via upload

---
+++ lab03oop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PositiveElementIndexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ініціалізація масиву з 20 елементів
+            int[] A = new int[20];
+            Random rnd = new Random();
+
+            // Заповнення масиву випадковими значеннями (-50 до 50)
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = rnd.Next(-50, 51);
+            }
+
+            // Виведення масиву
+            Console.WriteLine("Масив A:");
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine();
+
+            // Пошук індексів додатних елементів
+            Console.WriteLine("\nIндекси додатних елементiв масиву A:");
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] > 0)
+                {
+                    Console.WriteLine($"Iндекс {i}: значення {A[i]}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
